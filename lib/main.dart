d3ffbe4a576c48529b291ec3c7bbd6613961d6ee
Your Name <bhavikvashi1804@gmail.com>
2020-01-17 16:58:18 +0530

    Date display

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:intl/intl.dart';
+
 import './transaction.dart';
 
 void main() => runApp(MyApp());
@@ -49,7 +51,7 @@ class MyHomePage extends StatelessWidget {
                       Container(
                         margin: EdgeInsets.symmetric(horizontal: 15,vertical: 10),
                         padding: EdgeInsets.all(10),
-                        child: Text('\$ ${transaction.amount}',
+                        child: Text('₹${transaction.amount}',
                           style: TextStyle(color:Colors.purple,fontWeight: FontWeight.bold,fontSize: 20),
                         ),
                         decoration: BoxDecoration(border: Border.all(color: Colors.purple,width: 2,)),
@@ -58,7 +60,7 @@ class MyHomePage extends StatelessWidget {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
                           Text(transaction.title,style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),),
-                          Text(transaction.date.toString(),style: TextStyle(color: Colors.grey),),
+                          Text(DateFormat('dd MMM, yyyy hh:mm:ss aaa').format(transaction.date),style: TextStyle(color: Colors.grey),),
                         ],
                       ),
                     ],
