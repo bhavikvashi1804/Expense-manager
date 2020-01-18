fbb17e4af26085e42c0ba864b6f8fcff154d492d
Your Name <bhavikvashi1804@gmail.com>
2020-01-18 15:46:19 +0530

    still problem , you cannot use expanded widget

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -13,7 +13,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 400,
+      height: 500,
       child: _userTransactions.isEmpty
           ? Column(
               children: <Widget>[
