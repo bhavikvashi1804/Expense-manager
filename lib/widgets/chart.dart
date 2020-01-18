4d1a385aa529bd53b035a639998139aa88e87369
Your Name <bhavikvashi1804@gmail.com>
2020-01-18 12:18:14 +0530

    display font size correct

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
+import './chart_bar.dart';
 
 
 
@@ -30,7 +31,9 @@ class Chart extends StatelessWidget {
       }
 
 
-      
+      //print(DateFormat.E().format(weekDay).substring(0, 1));
+      //print(totalSum);
+
       return {
         'day': DateFormat.E().format(weekDay).substring(0, 1),
         'amount': totalSum,
@@ -38,16 +41,41 @@ class Chart extends StatelessWidget {
     });
   }
 
+
+  double get totalSpending{
+    return groupedTransactionValues.fold(
+      //first argument is starting value
+      0.0,
+      //second argument is function
+      (sum,item){
+        return sum+item['amount'];
+      }
+    );
+  }
   
 
   @override
   Widget build(BuildContext context) {
+    
     return Card(
       elevation: 6.0,
-      child: Row(
-        children: <Widget>[
-
-        ],
+      margin: EdgeInsets.all(20),
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues.map((data){
+            return Flexible(
+              fit: FlexFit.tight ,
+              child: ChartBar(
+                data['day'], 
+                data['amount'],
+                totalSpending==0.0? 0.0:(data['amount'] as double)/totalSpending
+                //if total spending is 0 then send 0
+              ),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
---
+++ lib/widgets/chart_bar.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+
+  final String label;
+  final double spendingAmount;
+  final double per;
+
+  ChartBar(this.label,this.spendingAmount,this.per);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        FittedBox(
+          //to solve problem : if one value is too large then that perticular bar goes to down 
+          //this box makes size of font smaller if content is large
+          child: Text('₹${spendingAmount.toStringAsFixed(0)}')
+        ),
+        SizedBox(
+          height: 4,
+        ),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey,width: 1),
+                  color: Color.fromRGBO(220, 220,220, 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              FractionallySizedBox(
+                //fraction size
+                heightFactor: per,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.purpleAccent,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  
+                ),
+
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 4,
+        ),
+        Text(label),
+
+      ],
+      
+    );
+  }
+}
