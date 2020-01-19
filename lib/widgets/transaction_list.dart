8818589e06061438e9e15e4f641a76198123deeb
Your Name <bhavikvashi1804@gmail.com>
2020-01-19 13:25:31 +0530

    display more is width of screen is more

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -56,7 +56,15 @@ class TransactionList extends StatelessWidget {
                     DateFormat.yMd().format(_userTransactions[index].date),
                     style: TextStyle(color: Colors.grey),
                   ),
-                  trailing: IconButton(
+                  trailing: MediaQuery.of(context).size.width>500?
+                  FlatButton.icon(
+                    icon: Icon(Icons.delete),
+                    textColor: Theme.of(context).errorColor,
+                    label: Text('Delete'),
+                    onPressed: () => deleteTx(_userTransactions[index].id),
+                  )
+                  :   
+                  IconButton(
                     icon: Icon(Icons.delete),
                     color: Theme.of(context).errorColor,
                     onPressed: () => deleteTx(_userTransactions[index].id),
